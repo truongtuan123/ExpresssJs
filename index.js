7a64aa2342eb7917ef9e59d169c14526a976e3a8
Trương Tuấn <ttuan2597@gmail.com>
2020-04-18 18:38:05 +0700

    Bài 4 - POST method

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+
+
 const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var users = [
     { id: 1, name: 'Lu Bo'},
@@ -30,6 +37,15 @@ app.get('/users/search', function(req, res){
         query: q,
         users: result
     })
+})
+
+app.get('/users/create', function(req, res) {
+    res.render('users/create');
+})
+
+app.post('/users/create', function(req, res){
+    users.push(req.body);
+    res.redirect('/users');
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
