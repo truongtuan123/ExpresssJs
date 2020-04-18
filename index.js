137f7e6bd641e32cd3a23305a019145db94398a4
Trương Tuấn <ttuan2597@gmail.com>
2020-04-18 14:55:55 +0700

    Bài 3 Query parameters

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+var users = [
+    { id: 1, name: 'Lu Bo'},
+    { id: 2, name: 'Quan Vu'}
+]
+
 app.get('/', function(req, res) {
     res.render('index', {
         name: "Truong Tuan"
@@ -12,11 +17,19 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res) {
     res.render('users/index', {
-        users: [
-            { id: 1, name: 'Lu Bo'},
-            { id: 2, name: 'Quan Vu'}
-        ]
+        users: users
     })
 });
+
+app.get('/users/search', function(req, res){
+    var q = req.query.q.toLowerCase();
+    var result = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q) !== -1;
+    })
+    res.render('users/index', {
+        query: q,
+        users: result
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
