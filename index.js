59add1fe62ff1ef5612a45d9a09051682dd7f3dc
Trương Tuấn <ttuan2597@gmail.com>
2020-04-18 14:36:09 +0700

    Pug

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('views', './views');
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    res.render('index', {
+        name: "Truong Tuan"
+    })
+})
+
+app.get('/users', function(req, res) {
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Lu Bo'},
+            { id: 2, name: 'Quan Vu'}
+        ]
+    })
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
