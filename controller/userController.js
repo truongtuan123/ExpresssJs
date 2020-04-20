f68c21379f2fd467adec722d95f351ce86f0f77f
Trương Tuấn <ttuan2597@gmail.com>
2020-04-20 10:20:37 +0700

    Bài 9 - Controller (MVC)

---
+++ controller/userController.js
@@ -0,0 +1,38 @@
+var db = require('../db');
+var shortid = require('shortid');
+
+module.exports.index = function (req, res) {
+    res.render('users/index', {
+        users: db.get('users').value()
+    })
+};
+
+module.exports.search = function (req, res) {
+    var q = req.query.q.toLowerCase();
+    var users = db.get('users').value();
+    var result = users.filter(function (user) {
+        return user.name.toLowerCase().indexOf(q) !== -1;
+    })
+    res.render('users/index', {
+        query: q,
+        users: result
+    })
+};
+
+module.exports.create = function (req, res) {
+    res.render('users/create');
+};
+
+module.exports.postCreate = function (req, res) {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+};
+
+module.exports.get = function(req, res){
+    var id = req.params.id;
+    var result = db.get('users').find({id: id}).value();
+    res.render('users/view', {
+        user: result
+    })
+};
--- router/user.route.js
+++ router/user.route.js
@@ -1,49 +1,22 @@
 var express = require('express');
-var shortid = require('shortid');
 var router = express.Router();
 
-var db = require('../db');
+var userController = require('../controller/userController');
 
 
-router.get('/', function(req, res) {
-    res.render('users/index', {
-        users: db.get('users').value()
-    })
-});
+router.get('/', userController.index);
 
 // Search APi
-router.get('/search', function(req, res){
-    var q = req.query.q.toLowerCase();
-    var users = db.get('users').value();
-    var result = users.filter(function(user){
-        return user.name.toLowerCase().indexOf(q) !== -1;
-    })
-    res.render('users/index', {
-        query: q,
-        users: result
-    })
-})
+router.get('/search', userController.search);
 
 // Trang create
-router.get('/create', function(req, res) {
-    res.render('users/create');
-})
+router.get('/create', userController.create);
 
 // Api create User
-router.post('/create', function(req, res){
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-})
+router.post('/create', userController.postCreate);
 
 
 // Get User
-router.get('/:id', function(req, res){
-    var id = req.params.id;
-    var result = db.get('users').find({id: id}).value();
-    res.render('users/view', {
-        user: result
-    })
-})
+router.get('/:id', userController.get);
 
 module.exports = router;
