d12ede92156f288e4d931db2938134d7ef9a4361
Trương Tuấn <ttuan2597@gmail.com>
2020-04-22 14:48:53 +0700

    Bài 20 - Pagination (Phân trang)

--- controller/productController.js
+++ controller/productController.js
@@ -2,7 +2,18 @@ var db = require('../db');
 var shortid = require('shortid');
 
 module.exports.index = function (req, res) {
+    var page = parseInt(req.query.page) || 1;
+    var perPage = 8;
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
+    if (start > db.get('products').value().length) {
+        page = 1;
+        start = (page - 1) * perPage;
+        end = page * perPage;
+    }
     res.render('product/index', {
-        products: db.get('products').value()
+        products: db.get('products').value().slice(start, end),
+        currentPage: page,
+        pages: [page, page + 1, page + 2]
     })
 };
